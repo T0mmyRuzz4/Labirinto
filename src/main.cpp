d02d79c8ede70818c03f4cc65e1dadc8f5dfd20e
T0mmyRuzz4 <tommaso.ruzzante04@gmail.com>
2024-12-05 12:38:49 +0100

    aggiunta cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
 
-#include "Maze.h"
-#include "Robot.h"
+#include "../include/Maze.h"
+#include "../include/Robot.h"
 
 int main(int argc, char *argv[]) {
 	if(argc != 2 || argv[1] != "maze.txt") {
-		std::cout << "Scrivere \"./main <maze.txt>\"\n";
+		std::cout << "Scrivere \"./main maze.txt\"\n";
 		return -1;
 	}
 	
