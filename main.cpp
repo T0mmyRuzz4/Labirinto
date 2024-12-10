882f613b33a31361f631ef1be44bf1c7575bae7c
T0mmyRuzz4 <tommaso.ruzzante04@gmail.com>
2024-12-10 16:14:35 +0100

    gestione matrice

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <string>
 
 #include "include/Maze.h"
 #include "include/Robot.h"
@@ -13,8 +14,8 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
-	std::string name;
-	int n;
+	std::string name;	// nome robot
+	int n;	// variabile per indicare il tipo di robot
 	std::cout << "Come vuoi chiamare il tuo robot?\n";
 	std::cin >> name;
 	do {
@@ -23,37 +24,36 @@ int main(int argc, char *argv[]) {
 		std::cin >> n;
 	} while(n != 1 && n != 2);
 
-	std::vector<char> list = std::vector<char>(9 * 9); // lista da trasformare
-
+	std::vector<char> list;
+	std::vector<std::vector<char>> labr;	// matrice
 	std::fstream fs("maze.txt");
 	char c;
-	int i;
-	while(fs.get(c))
-		list[i++] = c;
+	while(fs.get(c)) {
+		if(c == '\n') {
+			labr.push_back(list);
+			list.clear();
+		}
+		else
+			list.push_back(c);
+	}
 	fs.close();
 
-	std::vector<std::vector<char>> labr; // matrice
-	int t;
-	for(int i = 0; i < 9; i++) {
-		for(int j = 0; j < 9; j++)
-			labr[i][j] = list[t++];
-	}
-
-	Maze m = Maze(labr);
-	// if che suddivide il programma in base al robot scielto
+	Maze m = Maze(labr);	// creazione labirinto
+	// if che suddivide il programma in base al robot scelto
 	if(n == 1) {
 		RandomRobot r;
 		do {
 			r.move(m);
 		} while(!(r.finish(m)));
 	}
-
 	else if(n == 2) {
 		RightHandRuleRobot r;
 		do {
 			r.move(m);
 		} while(!(r.finish(m)));
 	}
+
+	std::cout << "EVVIVA!\n" << name << " ha trovato l'uscita.\n";
 
 	return 0;
 }
