f65eab81eb466808b475af479953109c8927ce32
T0mmyRuzz4 <tommaso.ruzzante04@gmail.com>
2024-12-10 20:26:25 +0100

    sofferenza

---
+++ build/CMakeFiles/RightHandRuleRobot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RightHandRuleRobot.dir/src/RightHandRuleRobot.cpp.obj"
+  "CMakeFiles/RightHandRuleRobot.dir/src/RightHandRuleRobot.cpp.obj.d"
+  "libRightHandRuleRobot.a"
+  "libRightHandRuleRobot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RightHandRuleRobot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
